3ff8a25b5641902064569171f91034517735a9a5
andikaarbillah <chrandika3@gmail.com>
2024-05-16 13:15:12 +0700

    binding

--- entity/video.go
+++ entity/video.go
@@ -1,7 +1,15 @@
 package entity
 
+type Person struct {
+	FirstName string `json:"firstname" binding:"required"`
+	LastName  string `json:"lastname" binding:"required"`
+	Age       int    `json:"age" binding:"gte=17,lte=56"`
+	Email     string `json:"email" binding:"required,email"`
+}
+
 type Video struct {
-	Tittle      string `json:"tittle"`
-	Description string `json:"description"`
-	URL         string `json:"url"`
+	Tittle      string `json:"tittle" binding:"min=2,max=22"`
+	Description string `json:"description" binding:"min=22,max=222"`
+	URL         string `json:"url" binding:"required,url"`
+	Author      Person `json:"author" binding:"required"`
 }
--- main.go
+++ main.go
@@ -21,16 +21,23 @@ func SetupLogOutput() {
 }
 func main() {
 	SetupLogOutput()
-	
+
 	server := gin.Default()
-	server.Use(gin.Recovery(), middleware.Logger(), middleware.BasicAuth())
+	server.Use(gin.Recovery(), middleware.Logger())
 
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
 	})
 
 	server.POST("/video", func(ctx *gin.Context) {
-		ctx.JSON(200, videoController.Save(ctx))
+		err := videoController.Save(ctx)
+		if err != nil {
+			ctx.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+		} else {
+			ctx.JSON(200, gin.H{"message": "Video Input is Succes"})
+		}
 	})
 
 	server.GET("/", func(ctx *gin.Context) {
